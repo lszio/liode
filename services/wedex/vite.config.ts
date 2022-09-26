d19e26ba14aa8f85789d202ac53f3983fb06ae85
liszt21 <liszt21@qq.com>
2022-09-26 21:09:27 +0800

    feat(wedex): use @crxjs/vite-plugin

--- services/wedex/vite.config.ts
+++ services/wedex/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {crx} from "@crxjs/vite-plugin"
+import manifest from "./manifest.json"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  build: {
+    rollupOptions: {
+      input: {
+        manifest: "manifest.json"
+      }
+    }
+  },
+  plugins: [react(), crx({manifest})]
 })
