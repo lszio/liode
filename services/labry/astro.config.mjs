ca2f348984a5683769aa9d1ee3cb3dbd28aba06a
liszt21 <liszt21@qq.com>
2023-06-08 09:31:59 +0800

    feat(labry): add react

--- services/labry/astro.config.mjs
+++ services/labry/astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config'; // https://astro.build/config
 
 import mdx from "@astrojs/mdx"; // https://astro.build/config
 
-import vue from "@astrojs/vue";
+import vue from "@astrojs/vue"; // https://astro.build/config
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), vue()]
+  integrations: [mdx(), vue(), react()]
 });
