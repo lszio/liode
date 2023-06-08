d5d529cad97b30ad6d338f871a91fe81661394ee
liszt21 <liszt21@qq.com>
2023-06-08 09:30:22 +0800

    feat(labry): add vue

--- services/labry/astro.config.mjs
+++ services/labry/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'; // https://astro.build/config
 
-import mdx from "@astrojs/mdx";
+import mdx from "@astrojs/mdx"; // https://astro.build/config
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), vue()]
 });
