9474eeb577efd6719826b591ade7a43a3dceea81
liszt21 <liszt21@qq.com>
2023-06-20 23:08:20 +0800

    feat(likit): slice for array

--- packages/likit/src/getters.spec.ts
+++ packages/likit/src/getters.spec.ts
@@ -1,12 +1,13 @@
 import { describe, expect, test, vi as spy } from "vitest"
 import { EditType, getEditOperations, withDifferents } from "./likit"
-import { getBy, parseGetter } from "./getters"
+import { slice, getBy, parseGetter, withLimit } from "./getters"
 
 const object = {
   o: { o: { n: 1 } },
   l: [{ o: { b: true } }, { s: 's' }],
   s: "s",
   b: false,
+  ll: [1, 2, 3, 4, 5]
 }
 
 describe("getters", () => {
@@ -15,9 +16,17 @@ describe("getters", () => {
     ["b", ["b"]],
     ["o.o.n", ["o", "o", "n"]],
     ["l[0].o.b", ["l", 0, "o", "b"]],
-    ["l(s@s)", ["l", "s"]]
+    ["l(s@s).s", ["l", "s", "s"]],
+    ["ll[::-1]", ["ll", "::-1"]]
   ])("parseGetter %s toBe %s", (s, gs) => {
-    expect(parseGetter(s).map(i => i["key"] || i["slot"] || i["index"])).toEqual(gs)
+    const key = (i: any) => {
+      if (i.key) return i.key
+      else if (i.slot) return i.slot
+      else if (i.start || i.end || i.step) return `${i["start"] || ''}:${i["end"] || ''}:${i["step"] || ''}`
+      else if (i.index !== undefined) return i.index
+      return undefined
+    }
+    expect(parseGetter(s).map(key)).toEqual(gs)
   })
 
   test.each([
@@ -26,8 +35,43 @@ describe("getters", () => {
     ["o.o.n", 1],
     ["l[0]", { o: { b: true } }],
     ["l[0].o.b", true],
-    ["l(s@s).s", "s"]
+    ["l(s@s).s", "s"],
+    ["ll[::-1]", [5, 4, 3, 2, 1]],
+    ["ll[0:-1]", [1, 2, 3, 4]],
+    ["ll[:-1]", [1, 2, 3, 4]],
+    ["ll[2:4]", [3, 4]],
+    ["ll[4:2:-1]", [5, 4]]
   ])("getBy(%s) toBe %s", (s, v) => {
     expect(getBy(object, ...parseGetter(s))).toEqual(v)
+  })
+
+  test.each([
+    [[1, 2, 3, 4, 5], undefined, undefined, undefined, [1, 2, 3, 4, 5]],
+    [[1, 2, 3, 4, 5], 0, undefined, undefined, [1, 2, 3, 4, 5]],
+    [[1, 2, 3, 4, 5], undefined, 5, undefined, [1, 2, 3, 4, 5]],
+    [[1, 2, 3, 4, 5], undefined, undefined, 1, [1, 2, 3, 4, 5]],
+    [[1, 2, 3, 4, 5], 2, undefined, undefined, [3, 4, 5]],
+    [[1, 2, 3, 4, 5], -3, undefined, undefined, [3, 4, 5]],
+    [[1, 2, 3, 4, 5], -33, undefined, undefined, [1, 2, 3, 4, 5]],
+    [[1, 2, 3, 4, 5], undefined, 2, undefined, [1, 2]],
+    [[1, 2, 3, 4, 5], undefined, 2, undefined, [1, 2]],
+    [[1, 2, 3, 4, 5], -4, -2, undefined, [2, 3]],
+    [[1, 2, 3, 4, 5], undefined, undefined, 2, [1, 3, 5]],
+    [[1, 2, 3, 4, 5], undefined, undefined, -1, [5, 4, 3, 2, 1]],
+    [[1, 2, 3, 4, 5], undefined, undefined, -1, [5, 4, 3, 2, 1]],
+    [[1, 2, 3, 4, 5], -1, undefined, -1, [5, 4, 3, 2, 1]],
+    [[1, 2, 3, 4, 5], undefined, 0, -1, [5, 4, 3, 2]],
+    [[1, 2, 3, 4, 5], undefined, undefined, 2, [1, 3, 5]],
+    [[1, 2, 3, 4, 5], undefined, undefined, -2, [5, 3, 1]],
+  ])("slice %s[%s:%s:%s] => %s", (array, start, end, step, sliced) => {
+    expect(slice(array, start, end, step)).toEqual(sliced)
+  })
+
+  test.each([
+    [11, 0, 100, 11],
+    [-11, 0, 100, 0],
+    [111, 0, 100, 100],
+  ])("withLimit %d, [%d, %d] => %d", (n, l, u, t) => {
+    expect(withLimit(n, l, u)).toBe(t)
   })
 })
