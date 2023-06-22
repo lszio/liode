140fe4ad326c660317ffb4908b52c1df5828b965
liszt21 <liszt21@qq.com>
2023-06-22 16:55:21 +0800

    feat(algro): add orgajs

--- services/algro/astro.config.mjs
+++ services/algro/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import org from "@orgajs/astro"
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  integrations: [org({}), mdx(), sitemap()],
 });
