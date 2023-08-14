95a278a0b07bf0fa43b5c3947e4841d69670c7f7
liszt21 <liszt21@qq.com>
2023-08-14 17:49:27 +0800

    fix(wedex): disable sandbox

--- services/wedex/src/crx/sw.cljs
+++ services/wedex/src/crx/sw.cljs
@@ -1,4 +1,29 @@
-(ns crx.sw)
+(ns crx.sw
+  [:require [shadow.cljs.modern :refer (js-await)]])
+
+(defn onMessage [event]
+  (js/console.log event))
+
+;; (defn ^:dev/before-load stop []
+;;   (js/removeEventListener onMessage))
+
+;; (defn open-side-panel [windowId])
+
+(defn get-windows []
+  #_{:clj-kondo/ignore [:unresolved-symbol]}
+  (js-await [windows (js/chrome.windows.getAll)]
+            (js/console.log windows)))
+
+(defn open-index-tab []
+  (js/chrome.runtime.onInstalled.addListener
+   (fn []
+     (js/chrome.tabs.create #js {:url "index.html" :pinned true :active true :index 0}))))
+
+(defn start []
+  (open-index-tab)
+  (get-windows)
+  (js/addEventListener "message" onMessage))
 
 (defn init []
-  (prn "sw"))
+  (prn "sw")
+  (start))
