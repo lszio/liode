23bd48549c72833721aa2b5daf2b2c96eb8a66ad
liszt21 <liszt21@qq.com>
2023-09-05 09:52:54 +0800

    feat: add vercel adapter

--- services/labry/astro.config.mjs
+++ services/labry/astro.config.mjs
@@ -4,9 +4,16 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
 import elm from "astro-integration-elm"
 
 export default defineConfig({
   integrations: [mdx(), vue(), react(), elm(), tailwind()],
-  output: "server"
+  output: "server",
+  adapter: vercel(),
+  vite: {
+    ssr: {
+      noExternal: ["linkedom"] // FIXME: for elm deps
+    }
+  }
 });
--- services/labry/src/components/LoginOrRegister.tsx
+++ services/labry/src/components/LoginOrRegister.tsx
@@ -1,4 +1,4 @@
-import { supabase } from "../supabase";
+import { supabase } from "../libs/supabase";
 
 async function signInWithGithub() {
   const { data, error } = await supabase.auth.signInWithOAuth({
