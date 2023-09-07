cda1d432a38293c98166724339eb04be4a3ddc67
liszt21 <liszt21@qq.com>
2023-09-07 18:54:43 +0800

    refactor: mv algro to labry

--- services/labry/astro.config.mjs
+++ services/labry/astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+import org from "@orgajs/astro"
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind"
+
+import sitemap from "@astrojs/sitemap"
 import vercel from "@astrojs/vercel/serverless"
 
 export default defineConfig({
-  integrations: [mdx(), vue(), react(), tailwind()],
+  site: "https://labry.vercel.app",
+  integrations: [mdx(), org({}), vue(), react(), tailwind(), sitemap()],
   output: "server",
   adapter: vercel(),
   vite: {
