57c134bcf7f6a2eea3eb2afa225d6bc61c608890
liszt21 <liszt21@qq.com>
2023-10-05 19:07:56 +0800

    feat: update layout

--- services/labry/src/store/user.ts
+++ services/labry/src/store/user.ts
@@ -2,3 +2,4 @@ import { atom } from "nanostores";
 
 export const $user = atom<any>(null);
 export const $session = atom<any>(null);
+export const $menuFolded = atom<boolean>(localStorage.getItem("menu-folded") === "t" ? true : false);
