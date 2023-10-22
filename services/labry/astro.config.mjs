dd20b95223398e81a0c7612649d2f79b364052db
liszt21 <liszt21@qq.com>
2023-10-22 23:08:11 +0800

    feat: use nodejs adapter default

--- services/labry/astro.config.mjs
+++ services/labry/astro.config.mjs
@@ -2,20 +2,35 @@ import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
 import org from "@orgajs/astro"
-import react from "@astrojs/react";
 import vue from "@astrojs/vue";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 
-import sitemap from "@astrojs/sitemap"
+import nodejs from "@astrojs/node"
 import vercel from "@astrojs/vercel/serverless"
+import { loadEnv } from "vite"
+const { ADAPTER_TYPE } = loadEnv(process.env.NODE_ENV, process.cwd(), "")
+
+let adapter;
+
+switch (ADAPTER_TYPE) {
+  case "vercel":
+    adapter = vercel({
+      functionPerRoute: false
+    })
+    break;
+  default:
+    adapter = nodejs({
+      mode: "standalone"
+    })
+}
 
 export default defineConfig({
   site: "https://labry.vercel.app",
   integrations: [mdx(), org({}), vue(), react({}), tailwind(), sitemap()],
   output: "server",
-  adapter: vercel({
-    functionPerRoute: false
-  }),
+  adapter,
   vite: {
     ssr: {
       noExternal: ["react-icons",]
