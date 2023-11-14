8fa7beda2403865b035b7c40601a646804b917d2
liszt21 <liszt21@qq.com>
2023-11-14 23:09:42 +0800

    feat: config rui

--- packages/rui/vite.config.ts
+++ packages/rui/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "rui",
+      formats: ["es"],
+      fileName: "index",
+    },
+  },
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+    }),
+  ],
+});
