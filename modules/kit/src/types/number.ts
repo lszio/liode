963420e19a63ab84f8179fcc27a9b589150744a5
lszio <lszio@qq.com>
2024-07-16 21:03:16 +0800

    feat: add number types

--- modules/kit/src/types/number.ts
+++ modules/kit/src/types/number.ts
@@ -12,3 +12,9 @@ export type Sub<A extends number, B extends number> = GetTuple<A> extends [
 ]
   ? R["length"]
   : never;
+
+type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>
+
+export type IntegerRange<S extends number, E extends number> = Exclude<Enumerate<E>, Enumerate<S>>;
